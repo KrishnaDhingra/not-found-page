3b7c017899f34923fc206cdd81da46095404904b
Krishna Dhingra <dkrishna1608@gmail.com>
2021-09-26 07:25:18 +0530

    Putting the image instead of the text

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import './index.css'
 import Navbar from './components/navbar.js'
+import NotFoundImage from './assets/not-found-image.png'
 
 function App() {
   return (
-    <div className="App h-screen bg-gray-200">
+    <div className="overflow-hidden App h-screen bg-gray-200">
 
       <Navbar/>
 
-        <div className="my-auto h-3/4 flex flex-col justify-center items-center gap-4">
+        <div className="inner-container grid place-items-center">
+          
+          <img className="not-found-image" src={NotFoundImage} alt="" />
 
-          <div className="flex flex-gap-2">
-            <p className="four font-semibold text-blue-500">4</p>
-            <p className="four font-semibold text-blue-500">0</p>
-            <p className="four font-semibold text-blue-500">4</p>
-          </div>
 
-          <p className="not-found-text text-black text-3xl text-center font-normal">The page you are looking for could not be found!</p>
+          <div className="flex flex-col gap-3">
 
-          <div className="flex gap-2">
+            <p className="not-found-text text-black text-3xl text-center font-normal">The page you are looking for could not be found!</p>
 
-            <span className="text-black text-xl font-normal">Please contact our </span>
-            <a href="#" className="text-blue-500 text-xl font-normal underline hover:text-blue-600">server mods</a>
-            <span className="text-black text-xl font-normal">for more info</span>
+            <div className="flex gap-2 justify-center">
+
+              <span className="text-black text-xl font-normal">Please contact our </span>
+              <a href="#" className="text-blue-500 text-xl font-normal underline hover:text-blue-600">server mods</a>
+              <span className="text-black text-xl font-normal">for more info</span>
+
+            </div>
 
           </div>
 
